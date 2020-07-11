55053af9bef5d7fdca2ca91a15cc9acf99a8f89c
Valto Moisio <valto.moisio@pilvit.fi>
2020-07-11 16:33:30 +0300

    Puhelinluettelo: complete step1

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Persons = ({ persons }) => (
+  <div>
+    {persons.map((item, i) => (
+      <p key={i}>{item.name}</p>
+    ))}
+  </div>
+);
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addName = (e) => {
+    e.preventDefault();
+    const newPerson = {
+      name: newName
+    };
+    setPersons(persons.concat(newPerson));
+  }
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Persons persons={persons} />
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App
