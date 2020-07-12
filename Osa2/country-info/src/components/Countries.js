b52059dacd8bdd2991e58540e4de0ff178d1081d
Valto Moisio <valto.moisio@pilvit.fi>
2020-07-12 17:51:01 +0300

    Country-info:complete step2

--- Osa2/country-info/src/components/Countries.js
+++ Osa2/country-info/src/components/Countries.js
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import CountryData from './CountryData';
 
 const Countries = ({ countries, countryFilter }) => {
+    const [selectedCountry, setSelectedCountry] = useState(null);
+
+    // set selectedCountry = null when countryFilter changes
+    useEffect(() => {
+        setSelectedCountry(null);
+    }, [countryFilter])
+
     let searchResult = countries.filter(country =>
         country.name.toLowerCase().includes(countryFilter.toLowerCase()));
+        console.log(selectedCountry);
+        
+    // Check if selectedCountry != null/undefined or a string matching exactly to filter is found
+    const exactName = selectedCountry ?
+    selectedCountry :
+    searchResult.find(
+        country =>
+        country.name.toLowerCase() === countryFilter.toLowerCase()
+    );
 
-    // Check if a string matching exactly to filter is found
-    const exactName = searchResult.find(country => country.name.toLowerCase() === countryFilter.toLowerCase());
     if (exactName) {
         searchResult = [exactName];
     }
 
+    const selectCountry = (country) => {
+        setSelectedCountry(country);
+    }
+
     let content = <div>Too many matches, specify another filter.</div>;
     if (searchResult.length <= 10 && searchResult.length !== 1) {
+        const inlineElement = {
+            display: 'inline-block',
+            marginRight: '8px'
+        }
+
         content = <div>{searchResult.map((country, i) => (
-            <p key={i}>{country.name}</p>
+            <div key={i}>
+                <p style={inlineElement}>{country.name}</p>
+                <button style={inlineElement} onClick={() => selectCountry(country)}>Show info</button>
+            </div>
         ))}</div>;
     }
     else if (searchResult.length === 1) {
+        
         content = <CountryData country={searchResult[0]} />;
     }
 
